01b05a6d4b16ba8b660d3bd61a53c436c431147a
DESKTOP-RS1PD2H\admin <tf-sjas@tobii.com>
2018-06-13 08:09:29 -0400

    edge setup complete

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -3,8 +3,30 @@ import { Link } from 'react-router-dom';
 
 let styles = require('./Home.scss');
 
+const path = require('path');
+const baseNetAppPath = path.join(path.resolve('.'), '\\app\\dotnet\\QuickStart.Core\\bin\\Debug\\netcoreapp2.0');
+
+process.env.EDGE_USE_CORECLR = '1';
+process.env.EDGE_APP_ROOT = baseNetAppPath;
+
+console.log('EDGE_APP_ROOT', process.env.EDGE_APP_ROOT);
+console.log('EDGE_BOOTSTRAP_DIR', process.env.EDGE_BOOTSTRAP_DIR);
+console.log('USE_CORE_CLR', process.env.EDGE_USE_CORECLR);
+
+var edge = require('electron-edge-js');
+
+var useDynamicInput = edge.func({
+  assemblyFile: path.join(baseNetAppPath, 'QuickStart.Core.dll'),
+  typeName: 'QuickStart.Core.LocalMethods',
+  methodName: 'UseDynamicInput'
+});
+
 export default class Home extends React.Component {
   render() {
+    useDynamicInput('HELLO ELECTRON', (error: any, result: any) => {
+      console.log('RESULT:', result);
+    });
+
     return (
       <div>
         <div className={styles.container} data-tid="container">
